6876dd7095a1466b1e00c1effbf8732699594e4d
ezisberta <ezisberta@gmail.com>
2022-11-08 10:50:26 +0000

    write tests for GET cards/cardId sucsess

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -6,49 +6,64 @@ describe("GET /cards/:cardId sucsess", () => {
     const response = await request(app).get("/cards/card003");
     expect(response.status).toBe(200);
   });
-
-  // test("returns matching card name", async () => {
-  //   const response = await request(app).get("/cards/card003");
-  //   expect(response.body).toEqual(
-  //     expect.objectContaining({ name: "card 3 name" })
-  //   );
-  // });
-  // test("returns matching card id", async () => {
-  //   const response = await request(app).get("/cards/card003");
-  //   expect(response.body).toEqual(expect.objectContaining({ id: "card003" }));
-  // });
-  // test("returns matching sales", async () => {
-  //   const response = await request(app).get("/cards/card003");
-  //   expect(response.body).toEqual(expect.objectContaining({ sales: 400 }));
-  // });
-  // test("returns matching piece", async () => {
-  //   const response = await request(app).get("/cards/card003");
-  //   expect(response.body).toEqual(expect.objectContaining({ piece: "hat" }));
-  // });
-  // test("returns matching outfits", async () => {
-  //   const response = await request(app).get("/cards/card003");
-  //   expect(response.body).toEqual(
-  //     expect.objectContaining({
-  //       outfit: [
-  //         { part: "head", garnmentId: "garnment006" },
-  //         { part: "waist", garnmentId: "garnment007" },
-  //         { part: "ankle", garnmentId: "garnment007" },
-  //         { part: "feet", garnmentId: "garnment008" },
-  //       ],
-  //     })
-  //   );
-  // });
-  // test("returns matching colours", async () => {
-  //   const response = await request(app).get("/cards/card003");
-  //   expect(response.body).toEqual(
-  //     expect.objectContaining({
-  //       availableColours: [
-  //         { id: "bk", title: "Black" },
-  //         { id: "br", title: "Brown" },
-  //       ],
-  //     })
-  //   );
-  // });
+  test("returns matching card title", async () => {
+    const response = await request(app).get("/cards/card003");
+    expect(response.body).toEqual(
+      expect.objectContaining({ title: "card 3 title" })
+    );
+  });
+  test("returns matching card id", async () => {
+    const response = await request(app).get("/cards/card003");
+    expect(response.body).toEqual(
+      expect.objectContaining({ card_id: "card003" })
+    );
+  });
+  test("returns matching base price", async () => {
+    const response = await request(app).get("/cards/card003");
+    expect(response.body).toEqual(expect.objectContaining({ base_price: 200 }));
+  });
+  test("returns matching imageUrl", async () => {
+    const response = await request(app).get("/cards/card003");
+    expect(response.body).toEqual(
+      expect.objectContaining({ imageUrl: "/front-cover-landscape.jpg" })
+    );
+  });
+  test("returns matching pages", async () => {
+    const response = await request(app).get("/cards/card003");
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        pages: [
+          {
+            title: "Front Cover",
+            templateId: "template006",
+          },
+          {
+            title: "Inside Top",
+            templateId: "template007",
+          },
+          {
+            title: "Inside Bottom",
+            templateId: "template007",
+          },
+          {
+            title: "Back Cover",
+            templateId: "template008",
+          },
+        ],
+      })
+    );
+  });
+  test("returns matching sizes", async () => {
+    const response = await request(app).get("/cards/card003");
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        availableSizes: [
+          { id: "md", title: "Medium" },
+          { id: "lg", title: "Large" },
+        ],
+      })
+    );
+  });
 });
 
 // test('returns matching card title', async () => {
--- src/controllers.ts
+++ src/controllers.ts
@@ -3,4 +3,5 @@ const { fetchCardById } = require("./models");
 exports.getCardById = (req, res, next) => {
   console.log("CONTROLLER");
   fetchCardById();
+  res.send({});
 };
